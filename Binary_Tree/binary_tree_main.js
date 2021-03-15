a96d958997c770b4e224c37b07deff9e146aad72
codingwith3dv <deveshdas3dv@gmail.com>
2021-03-15 07:42:50 +0530

    hello1

--- Binary_Tree/binary_tree_main.js
+++ Binary_Tree/binary_tree_main.js
@@ -1,18 +1,26 @@
 var insertButton = document.getElementById('insert')
 var container = document.getElementById('treeContainer')
-var svg = "http://www.w3.org/2000/svg";  
+var svg = "http://www.w3.org/2000/svg";
+var binary_array_length = 20;
+var binary_array = [binary_array_length]
 
-for(var i = 0; i < 10; i++){
-    console.log(i);
-    var circle = document.createElementNS(svg, 'circle')
-    var x = Math.floor(Math.random() * ((window.innerWidth-20) -20+1))+20
-    circle.setAttributeNS(null, 'cx',String(x))
-    var y = Math.floor(Math.random() * (100-20+1))+20
-    circle.setAttributeNS(null, 'cy', String(y))
-    console.log(x + '    ' + y);
-    circle.setAttributeNS(null, 'r',String(10))
-    circle.setAttributeNS(null, 'fill','white')
-    circle.setAttributeNS(null, 'stroke', 'black')
-    circle.setAttributeNS(null, 'stroke-width', String(2))
-    container.appendChild(circle)
+function createNode(x, y, index) {
+    binary_array[index] = document.createElementNS(svg, 'circle')
+    binary_array[index].setAttributeNS(null, 'cx', x)
+    binary_array[index].setAttributeNS(null, 'cy', y)
+    binary_array[index].setAttributeNS(null, 'fill', 'red')
+    binary_array[index].setAttributeNS(null, 'r', 10)
+    binary_array[index].style='transition: 0.5s all ease-in-out;'
+    container.appendChild(binary_array[index])
 }
+
+function updateNode(color,index){
+    binary_array[index].setAttribute('fill', color )
+}
+
+window.onload = function(){
+    createNode(50,50,0)
+    createNode(100,100,1)
+}
+
+updateNode('green',0)
