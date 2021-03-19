a7b0e5d884fdd3269981db80d60684981bd9b102
codingwith3dv <deveshdas3dv@gmail.com>
2021-03-19 22:33:24 +0530

    hello1

--- Linked_List/linked_list_main.js
+++ Linked_List/linked_list_main.js
@@ -1,6 +1,6 @@
 var linkedlist = new LinkedList();
 
-document.getElementById('insertll').onclick = async() => {
+document.getElementById('appendll').onclick = async() => {
     var value = document.getElementById('inputnumll').value;
     value = parseInt(value)
     linkedlist.add(nodes.length, value)
